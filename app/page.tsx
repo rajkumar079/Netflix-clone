7322988b26cf511c53b7d90660724552227dab08
rajkumar079 <rajkumarmaddi079@gmail.com>
2023-12-12 15:19:27 +0530

    finished project

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import { authOptions } from "./utils/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/login");
+  } else {
+    return redirect("/home");
+  }
+}
